a65950398f7b16ff2247fd90c2a22880fe3e4905
CHATELAIN JORDAN <jordan139@hotmail.fr>
2026-01-21 13:33:36 +0100

    Ajustements finaux CSS notifications et menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
   Application Planning PWA
 */
-export const APP_VERSION = "1.0.40";
+export const APP_VERSION = "1.0.42";
 
 import { registerServiceWorker } from "./sw/sw-register.js";
 import { initServicesIfNeeded } from "./data/services-init.js";
@@ -89,6 +89,8 @@ function showUpdateBanner() {
     banner.remove();
   };
 }
+
+
 
 
 
